c2fb978a586c6365846317a09e401dd864164e1a
hungvm <hungvimanh.cntt@gmail.com>
2020-04-07 11:14:40 +0700

    fix status id

--- DMS/Enums/StatusEnum.cs
+++ DMS/Enums/StatusEnum.cs
@@ -4,7 +4,7 @@ namespace DMS.Enums
 {
     public class StatusEnum
     {
-        public static GenericEnum ACTIVE = new GenericEnum { Id = 0, Code = "Active", Name = "Hoạt động" };
-        public static GenericEnum INACTIVE = new GenericEnum { Id = 1, Code = "Inactive", Name = "Dừng hoạt động" };
+        public static GenericEnum ACTIVE = new GenericEnum { Id = 1, Code = "Active", Name = "Hoạt động" };
+        public static GenericEnum INACTIVE = new GenericEnum { Id = 0, Code = "Inactive", Name = "Dừng hoạt động" };
     }
 }
