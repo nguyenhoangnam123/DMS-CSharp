a193e708d8ca402bdd5d8d653dc4ccb4cc35b111
thangld <leducthang1991@gmail.com>
2020-04-09 04:12:10 +0700

    đổi về port 80

--- DMS/docker-entrypoint.sh
+++ DMS/docker-entrypoint.sh
@@ -5,7 +5,7 @@ fi
 
 PROJECT_NAME="DMS"
 
-dotnet ${PROJECT_NAME}.dll --urls http://127.0.0.1:10002 --environment Development --launch-profile ${PROJECT_NAME} &
+dotnet ${PROJECT_NAME}.dll --urls http://localhost:80 --environment Development &
 consul agent -config-dir /consul/config -node ${NODE}&
 sleep 5
 consul connect envoy -sidecar-for dms-backend
