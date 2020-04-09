5244e94f7ad411274a3a4ef2136ba0de7e3aa1b3
thangld <leducthang1991@gmail.com>
2020-04-09 14:56:47 +0700

    bổ sung cấu hình cho consul

--- DMS/docker-entrypoint.sh
+++ DMS/docker-entrypoint.sh
@@ -3,10 +3,23 @@ if [ -z ${NODE} ]; then
 	    NODE="dms-backend_${HOSTNAME}"
 fi
 
+# setting up SIGTERM handler for consul agent
+CONSUL_PID=0
+term_handler () {
+    if [ ${CONSUL_PID} -ne 0 ]; then
+        kill -SIGTERM "${CONSUL_PID}"
+        wait "${CONSUL_PID}"
+    fi
+    exit 143;
+}
+trap term_handler TERM
+
 PROJECT_NAME="DMS"
 
 dotnet ${PROJECT_NAME}.dll --urls http://localhost:80 --environment Development &
 consul agent -config-dir /consul/config -node ${NODE}&
-sleep 5
+CONSUL_PID="$!"
+sleep 10
 consul connect envoy -sidecar-for dms-backend
+wait "${CONSUL_PID}"
 
