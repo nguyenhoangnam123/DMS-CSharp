f349d6b0cc0af78f26a552e0b03b25471f370bf7
hungvm <hungvimanh.cntt@gmail.com>
2020-04-09 19:12:08 +0700

    product grouping trả thiếu trường, đã thêm

---
+++ DMS/Rpc/product-grouping/ProductGrouping_StatusDTO.cs
@@ -0,0 +1,43 @@
+﻿using Common;
+using DMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.product_grouping
+{
+    public class ProductGrouping_StatusDTO : DataDTO
+    {
+        public long Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+
+        public ProductGrouping_StatusDTO() { }
+        public ProductGrouping_StatusDTO(Status Status)
+        {
+
+            this.Id = Status.Id;
+
+            this.Code = Status.Code;
+
+            this.Name = Status.Name;
+
+        }
+    }
+
+    public class ProductGrouping_StatusFilterDTO : FilterDTO
+    {
+
+        public IdFilter Id { get; set; }
+
+        public StringFilter Code { get; set; }
+
+        public StringFilter Name { get; set; }
+
+        public StatusOrder OrderBy { get; set; }
+    }
+}
