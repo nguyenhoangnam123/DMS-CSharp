42a4d4d21df3b4ffafba1589dbd89734f7e5d800
hungvm <hungvimanh.cntt@gmail.com>
2020-04-23 14:53:31 +0700

    bổ xung validate cho nhóm sản phẩm

--- DMS/Services/MProductGrouping/ProductGroupingValidator.cs
+++ DMS/Services/MProductGrouping/ProductGroupingValidator.cs
@@ -1,7 +1,9 @@
 using Common;
 using DMS.Entities;
+using DMS.Enums;
 using DMS.Repositories;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DMS.Services.MProductGrouping
@@ -20,6 +22,13 @@ namespace DMS.Services.MProductGrouping
         public enum ErrorCode
         {
             IdNotExisted,
+            CodeEmpty,
+            CodeHasSpecialCharacter,
+            CodeExisted,
+            NameEmpty,
+            NameOverLength,
+            ParentNotExisted,
+            ProductGroupingInUsed
         }
 
         private IUOW UOW;
@@ -47,8 +56,70 @@ namespace DMS.Services.MProductGrouping
             return count == 1;
         }
 
+        private async Task<bool> ValidateCode(ProductGrouping ProductGrouping)
+        {
+            if (string.IsNullOrWhiteSpace(ProductGrouping.Code))
+            {
+                ProductGrouping.AddError(nameof(ProductGroupingValidator), nameof(ProductGrouping.Code), ErrorCode.CodeEmpty);
+            }
+            else
+            {
+                var Code = ProductGrouping.Code;
+                if (!FilterExtension.ChangeToEnglishChar(Code).Equals(ProductGrouping.Code))
+                {
+                    ProductGrouping.AddError(nameof(ProductGroupingValidator), nameof(ProductGrouping.Code), ErrorCode.CodeHasSpecialCharacter);
+                }
+
+                ProductGroupingFilter ProductGroupingFilter = new ProductGroupingFilter
+                {
+                    Skip = 0,
+                    Take = 10,
+                    Id = new IdFilter { NotEqual = ProductGrouping.Id },
+                    Code = new StringFilter { Equal = ProductGrouping.Code },
+                    Selects = ProductGroupingSelect.Code
+                };
+
+                int count = await UOW.ProductGroupingRepository.Count(ProductGroupingFilter);
+                if (count != 0)
+                    ProductGrouping.AddError(nameof(ProductGroupingValidator), nameof(ProductGrouping.Code), ErrorCode.CodeExisted);
+            }
+            return ProductGrouping.IsValidated;
+        }
+
+        private async Task<bool> ValidateName(ProductGrouping ProductGrouping)
+        {
+            if (string.IsNullOrWhiteSpace(ProductGrouping.Name))
+            {
+                ProductGrouping.AddError(nameof(ProductGroupingValidator), nameof(ProductGrouping.Name), ErrorCode.NameEmpty);
+            }
+            else if (ProductGrouping.Name.Length > 255)
+            {
+                ProductGrouping.AddError(nameof(ProductGroupingValidator), nameof(ProductGrouping.Name), ErrorCode.NameOverLength);
+            }
+            return ProductGrouping.IsValidated;
+        }
+
+        private async Task<bool> ValidateParent(ProductGrouping ProductGrouping)
+        {
+            ProductGroupingFilter ProductGroupingFilter = new ProductGroupingFilter
+            {
+                Skip = 0,
+                Take = 10,
+                Id = new IdFilter { Equal = ProductGrouping.ParentId },
+                Selects = ProductGroupingSelect.Id
+            };
+
+            int count = await UOW.ProductGroupingRepository.Count(ProductGroupingFilter);
+            if (count == 0)
+                ProductGrouping.AddError(nameof(ProductGroupingValidator), nameof(ProductGrouping.ParentId), ErrorCode.ParentNotExisted);
+            return count != 0;
+        }
+
         public async Task<bool> Create(ProductGrouping ProductGrouping)
         {
+            await ValidateCode(ProductGrouping);
+            await ValidateName(ProductGrouping);
+            await ValidateParent(ProductGrouping);
             return ProductGrouping.IsValidated;
         }
 
@@ -56,6 +127,9 @@ namespace DMS.Services.MProductGrouping
         {
             if (await ValidateId(ProductGrouping))
             {
+                await ValidateCode(ProductGrouping);
+                await ValidateName(ProductGrouping);
+                await ValidateParent(ProductGrouping);
             }
             return ProductGrouping.IsValidated;
         }
@@ -64,13 +138,26 @@ namespace DMS.Services.MProductGrouping
         {
             if (await ValidateId(ProductGrouping))
             {
+                ProductFilter ProductFilter = new ProductFilter
+                {
+                    ProductGroupingId = new IdFilter { Equal = ProductGrouping.Id },
+                    StatusId = new IdFilter { Equal = StatusEnum.ACTIVE.Id },
+                };
+
+                var count = await UOW.ProductRepository.Count(ProductFilter);
+                if (count > 0)
+                    ProductGrouping.AddError(nameof(ProductGroupingValidator), nameof(ProductGrouping.Id), ErrorCode.ProductGroupingInUsed);
             }
             return ProductGrouping.IsValidated;
         }
 
         public async Task<bool> BulkDelete(List<ProductGrouping> ProductGroupings)
         {
-            return true;
+            foreach (ProductGrouping ProductGrouping in ProductGroupings)
+            {
+                await Delete(ProductGrouping);
+            }
+            return ProductGroupings.All(st => st.IsValidated);
         }
 
         public async Task<bool> Import(List<ProductGrouping> ProductGroupings)
