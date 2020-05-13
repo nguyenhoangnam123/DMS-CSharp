c8bedc9da9130ea216f4ad3717eca1d46db5cf45
LeDucThang <leducthang1991@gmail.com>
2020-05-13 15:20:32 +0700

    update lại trạng thái của INACTIVE trong EditedPriceStatusEnum

--- DMS/Enums/EditedPriceStatusEnum.cs
+++ DMS/Enums/EditedPriceStatusEnum.cs
@@ -9,6 +9,6 @@ namespace DMS.Enums
     public class EditedPriceStatusEnum
     {
         public static GenericEnum ACTIVE = new GenericEnum { Id = 1, Code = "ACTIVE", Name = "Active" };
-        public static GenericEnum INACTIVE = new GenericEnum { Id = 2, Code = "INACTIVE", Name = "Inactive" };
+        public static GenericEnum INACTIVE = new GenericEnum { Id = 0, Code = "INACTIVE", Name = "Inactive" };
     }
 }
