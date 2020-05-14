dc902835419abe9ac40918a999796b6e98aa2c14
hungvm <hungvimanh.cntt@gmail.com>
2020-05-14 13:38:32 +0700

    trả thêm dữ liệu cho indirect

--- DMS/Repositories/DirectSalesOrderRepository.cs
+++ DMS/Repositories/DirectSalesOrderRepository.cs
@@ -512,13 +512,47 @@ namespace DMS.Repositories
                     Item = new Item
                     {
                         Id = x.Item.Id,
-                        ProductId = x.Item.ProductId,
                         Code = x.Item.Code,
                         Name = x.Item.Name,
-                        ScanCode = x.Item.ScanCode,
-                        SalePrice = x.Item.SalePrice,
+                        ProductId = x.Item.ProductId,
                         RetailPrice = x.Item.RetailPrice,
+                        SalePrice = x.Item.SalePrice,
+                        ScanCode = x.Item.ScanCode,
                         StatusId = x.Item.StatusId,
+                        Product = new Product
+                        {
+                            Id = x.Item.Product.Id,
+                            Code = x.Item.Product.Code,
+                            Name = x.Item.Product.Name,
+                            TaxTypeId = x.Item.Product.TaxTypeId,
+                            UnitOfMeasureId = x.Item.Product.UnitOfMeasureId,
+                            UnitOfMeasureGroupingId = x.Item.Product.UnitOfMeasureGroupingId,
+                            TaxType = new TaxType
+                            {
+                                Id = x.Item.Product.TaxType.Id,
+                                Code = x.Item.Product.TaxType.Code,
+                                Name = x.Item.Product.TaxType.Name,
+                                StatusId = x.Item.Product.TaxType.StatusId,
+                                Percentage = x.Item.Product.TaxType.Percentage,
+                            },
+                            UnitOfMeasure = new UnitOfMeasure
+                            {
+                                Id = x.Item.Product.UnitOfMeasure.Id,
+                                Code = x.Item.Product.UnitOfMeasure.Code,
+                                Name = x.Item.Product.UnitOfMeasure.Name,
+                                Description = x.Item.Product.UnitOfMeasure.Description,
+                                StatusId = x.Item.Product.UnitOfMeasure.StatusId,
+                            },
+                            UnitOfMeasureGrouping = new UnitOfMeasureGrouping
+                            {
+                                Id = x.Item.Product.UnitOfMeasureGrouping.Id,
+                                Code = x.Item.Product.UnitOfMeasureGrouping.Code,
+                                Name = x.Item.Product.UnitOfMeasureGrouping.Name,
+                                Description = x.Item.Product.UnitOfMeasureGrouping.Description,
+                                StatusId = x.Item.Product.UnitOfMeasureGrouping.StatusId,
+                                UnitOfMeasureId = x.Item.Product.UnitOfMeasureGrouping.UnitOfMeasureId
+                            }
+                        }
                     },
                     PrimaryUnitOfMeasure = new UnitOfMeasure
                     {
