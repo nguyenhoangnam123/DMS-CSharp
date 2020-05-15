e3ef09b5f978a34d7382390b0e3dd7ed17446cef
hungvm <hungvimanh.cntt@gmail.com>
2020-05-15 09:14:47 +0700

    map product và status cho item trong product

--- DMS/Rpc/product/Product_ItemDTO.cs
+++ DMS/Rpc/product/Product_ItemDTO.cs
@@ -14,9 +14,12 @@ namespace DMS.Rpc.product
         public string ScanCode { get; set; }
         public decimal? SalePrice { get; set; }
         public decimal? RetailPrice { get; set; }
+        public long SaleStock { get; set; }
         public bool CanDelete { get; set; }
         public bool HasInventory { get; set; }
         public long StatusId { get; set; }
+        public Product_ProductDTO Product { get; set; }
+        public Product_StatusDTO Status { get; set; }
         public List<Product_ItemImageMappingDTO> ItemImageMappings { get; set; }
         public Product_ItemDTO() { }
         public Product_ItemDTO(Item Item)
@@ -28,9 +31,12 @@ namespace DMS.Rpc.product
             this.ScanCode = Item.ScanCode;
             this.SalePrice = Item.SalePrice;
             this.RetailPrice = Item.RetailPrice;
+            this.SaleStock = Item.SaleStock;
             this.CanDelete = Item.CanDelete;
             this.HasInventory = Item.HasInventory;
             this.StatusId = Item.StatusId;
+            this.Product = Item.Product == null ? null : new Product_ProductDTO(Item.Product);
+            this.Status = Item.Status == null ? null : new Product_StatusDTO(Item.Status);
             this.ItemImageMappings = Item.ItemImageMappings?.Select(x => new Product_ItemImageMappingDTO(x)).ToList();
             this.Errors = Item.Errors;
         }
