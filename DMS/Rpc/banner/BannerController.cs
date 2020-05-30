581e4f439526c49124b00ceb75395738ecf28878
hungvm <hungvimanh.cntt@gmail.com>
2020-05-30 16:18:11 +0700

    build list image cho banner

--- DMS/Rpc/banner/BannerController.cs
+++ DMS/Rpc/banner/BannerController.cs
@@ -642,12 +642,12 @@ namespace DMS.Rpc.banner
                 Birthday = Banner_BannerDTO.Creator.Birthday,
                 ProvinceId = Banner_BannerDTO.Creator.ProvinceId,
             };
-            Banner.Image = Banner_BannerDTO.Image == null ? null : new Image
+            Banner.Image = Banner_BannerDTO.Images == null ? null : Banner_BannerDTO.Images.Select(x => new Image
             {
-                Id = Banner_BannerDTO.Image.Id,
-                Name = Banner_BannerDTO.Image.Name,
-                Url = Banner_BannerDTO.Image.Url,
-            };
+                Id = x.Id,
+                Name = x.Name,
+                Url = x.Url,
+            }).FirstOrDefault();
             Banner.Status = Banner_BannerDTO.Status == null ? null : new Status
             {
                 Id = Banner_BannerDTO.Status.Id,
