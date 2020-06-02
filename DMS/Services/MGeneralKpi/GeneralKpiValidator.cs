a4e52a5ab8661f09000d996e8d563c3dcb2ca69d
hungvm <hungvimanh.cntt@gmail.com>
2020-06-02 14:02:42 +0700

    commit tạm

--- DMS/Services/MGeneralKpi/GeneralKpiValidator.cs
+++ DMS/Services/MGeneralKpi/GeneralKpiValidator.cs
@@ -6,6 +6,8 @@ using Common;
 using DMS.Entities;
 using DMS;
 using DMS.Repositories;
+using DMS.Services.MAppUser;
+using DMS.Enums;
 
 namespace DMS.Services.MGeneralKpi
 {
@@ -23,6 +25,9 @@ namespace DMS.Services.MGeneralKpi
         public enum ErrorCode
         {
             IdNotExisted,
+            OrganizationIdNotExisted,
+            EmployeeIdsEmpty,
+            StatusNotExisted
         }
 
         private IUOW UOW;
@@ -48,6 +53,56 @@ namespace DMS.Services.MGeneralKpi
             if (count == 0)
                 GeneralKpi.AddError(nameof(GeneralKpiValidator), nameof(GeneralKpi.Id), ErrorCode.IdNotExisted);
             return count == 1;
+        }
+
+        private async Task<bool> ValidateOrganization(GeneralKpi GeneralKpi)
+        {
+            OrganizationFilter OrganizationFilter = new OrganizationFilter
+            {
+                Id = new IdFilter { Equal = GeneralKpi.OrganizationId }
+            };
+
+            var count = await UOW.OrganizationRepository.Count(OrganizationFilter);
+            if(count == 0)
+                GeneralKpi.AddError(nameof(GeneralKpiValidator), nameof(GeneralKpi.Organization), ErrorCode.OrganizationIdNotExisted);
+            return GeneralKpi.IsValidated;
+        }
+
+        private async Task<bool> ValidateEmployees(GeneralKpi GeneralKpi)
+        {
+            if(GeneralKpi.EmployeeIds == null || !GeneralKpi.EmployeeIds.Any())
+                GeneralKpi.AddError(nameof(GeneralKpiValidator), nameof(GeneralKpi.EmployeeIds), ErrorCode.EmployeeIdsEmpty);
+            else
+            {
+                AppUserFilter AppUserFilter = new AppUserFilter
+                {
+                    Skip = 0,
+                    Take = int.MaxValue,
+                    Id = new IdFilter { In = GeneralKpi.EmployeeIds },
+                    OrganizationId = new IdFilter(),
+                    Selects = AppUserSelect.Id
+                };
+
+                var EmployeeIdsInDB = (await UOW.AppUserRepository.List(AppUserFilter)).Select(x => x.Id).ToList();
+                var listIdsNotExisted = GeneralKpi.EmployeeIds.Except(EmployeeIdsInDB).ToList();
+
+                if(listIdsNotExisted != null && listIdsNotExisted.Any())
+                {
+                    foreach (var Id in listIdsNotExisted)
+                    {
+                        GeneralKpi.AddError(nameof(GeneralKpiValidator), nameof(GeneralKpi.EmployeeIds), ErrorCode.IdNotExisted);
+                    }
+                }
+                
+            }
+            return GeneralKpi.IsValidated;
+        }
+
+        public async Task<bool> ValidateStatus(GeneralKpi GeneralKpi)
+        {
+            if (StatusEnum.ACTIVE.Id != GeneralKpi.StatusId && StatusEnum.INACTIVE.Id != GeneralKpi.StatusId)
+                GeneralKpi.AddError(nameof(GeneralKpiValidator), nameof(GeneralKpi.Status), ErrorCode.StatusNotExisted);
+            return GeneralKpi.IsValidated;
         }
 
         public async Task<bool>Create(GeneralKpi GeneralKpi)
