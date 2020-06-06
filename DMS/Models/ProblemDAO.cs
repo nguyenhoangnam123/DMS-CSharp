6f0708c738fd11ad5743b5ee4a755853427acc81
LeDucThang <leducthang1991@gmail.com>
2020-06-06 14:13:26 +0700

    commmit

--- DMS/Models/ProblemDAO.cs
+++ DMS/Models/ProblemDAO.cs
@@ -13,10 +13,15 @@ namespace DMS.Models
         public long Id { get; set; }
         public long? StoreCheckingId { get; set; }
         public long StoreId { get; set; }
+        public long CreatorId { get; set; }
         public long ProblemTypeId { get; set; }
         public DateTime NoteAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
         public string Content { get; set; }
+        public long ProblemStatusId { get; set; }
 
+        public virtual AppUserDAO Creator { get; set; }
+        public virtual ProblemStatusDAO ProblemStatus { get; set; }
         public virtual ProblemTypeDAO ProblemType { get; set; }
         public virtual StoreDAO Store { get; set; }
         public virtual StoreCheckingDAO StoreChecking { get; set; }
