58e07a5fcb79325cb78eae598583afa3ab85f398
LeDucThang <leducthang1991@gmail.com>
2020-06-15 15:01:11 +0700

    các bảng mặc định order theo thứ tự giảm dần

--- DMS/Common/FilterEntity.cs
+++ DMS/Common/FilterEntity.cs
@@ -13,14 +13,14 @@ namespace Common
         {
             Skip = 0;
             Take = 10;
-            OrderType = OrderType.ASC;
+            OrderType = OrderType.DESC;
         }
     }
 
     [JsonConverter(typeof(StringEnumConverter))]
     public enum OrderType
     {
+        DESC,
         ASC,
-        DESC
     }
 }
