f5259daf0f1da97157ebae82ff6aa363d3c98092
hungvimanh <hungvimanh.cntt@gmail.com>
2020-06-17 15:37:18 +0700

    commit dashboard

--- DMS/Rpc/dashboards/DashboardRoute.cs
+++ DMS/Rpc/dashboards/DashboardRoute.cs
@@ -9,8 +9,11 @@ namespace DMS.Rpc.dashboards
     public class DashboardRoute : Root
     {
         private const string Default = Rpc + Module + "/dashboards";
+        public const string ListIndirectSalesOrder = Default + "/list-indirect-sales-order";
         public const string StoreChecking = Default + "/store-checking";
-        public const string IndirectSalesOrder = Default + "/indirect-sales-order";
+        public const string StatisticIndirectSalesOrder = Default + "/statistic-indirect-sales-order";
         public const string ImageStoreCheking = Default + "/image-store-checking";
+        public const string FilterListTime = Default + "/filter-list-time";
+        public const string TopSaleEmployeeStoreChecking = Default + "/top-sale-employee-store-checking";
     }
 }
---
+++ DMS/Rpc/dashboards/Dashboard_TopSaleEmployeeStoreCheckingDTO.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.dashboards
+{
+    public class Dashboard_TopSaleEmployeeStoreCheckingDTO : DataDTO
+    {
+        public long SaleEmployeeId { get; set; }
+        public string DisplayName { get; set; }
+        public long Counter { get; set; }
+    }
+
+    public class Dashboard_TopSaleEmployeeStoreCheckingFilterDTO : FilterDTO
+    {
+        public IdFilter Time { get; set; }
+    }
+}
