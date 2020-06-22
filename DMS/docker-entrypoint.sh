142bdceb81074e57e9197e1e6887eff4a13aa5e1
LeDucThang <leducthang1991@gmail.com>
2020-06-22 16:31:33 +0700

    update consul

--- DMS/docker-entrypoint.sh
+++ DMS/docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sed -i "s/{SECRET_KEY}/${SECRET_KEY}/g" appsettings.json
+sed -i "s/{SQL_DB}/${SQL_DB}/g" appsettings.json
+sed -i "s/{SQL_USER}/${SQL_USER}/g" appsettings.json
+sed -i "s/{SQL_PASS}/${SQL_PASS}/g" appsettings.json
+sed -i "s/{RB_USER}/${RB_USER}/g" appsettings.json
+sed -i "s/{RB_PASS}/${RB_PASS}/g" appsettings.json
+
 if [ -z ${NODE} ]; then
 	    NODE="dms-backend_${HOSTNAME}"
 fi
