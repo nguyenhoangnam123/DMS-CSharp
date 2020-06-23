7628a1973f3d9de1b88d9713f46c4ccc2f945026
LeDucThang <leducthang1991@gmail.com>
2020-06-23 15:13:57 +0700

    bổ sung init data từ es

---
+++ DMS/Enums/SexEnum.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Enums
+{
+    public class SexEnum
+    {
+        public static GenericEnum Male => new GenericEnum { Id = 1, Name = "Nam", Code = "Male" };
+        public static GenericEnum Female => new GenericEnum { Id = 2, Name = "Nữ", Code = "Female" };
+    }
+}
