ec8c76b148cb68e474fc14b0823a9c26637bfc96
hungvimanh <hungvimanh.cntt@gmail.com>
2020-07-13 17:51:22 +0700

    hoàn thiện báo cáo bán hàng theo nhân viên và sản phẩm

---
+++ DMS/Rpc/reports/report-sales-order/report-sales-order-by-employee-and-item/ReportSalesOrderByEmployeeAndItemRoute.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.reports.report_sales_order.report_sales_order_by_employee_and_item
+{
+    public class ReportSalesOrderByEmployeeAndItemRoute : Root
+    {
+        public const string Master = Module + "/report-sales-order-by-employee-and-item/report-sales-order-by-employee-and-item-master";
+
+        private const string Default = Rpc + Module + "/report-sales-order-by-employee-and-item";
+        public const string Count = Default + "/count";
+        public const string List = Default + "/list";
+        public const string Total = Default + "/total";
+        public const string Export = Default + "/export";
+
+        public const string FilterListOrganization = Default + "/filter-list-organization";
+        public const string FilterListAppUser = Default + "/filter-list-app-user";
+
+        public static Dictionary<string, long> Filters = new Dictionary<string, long>
+        {
+            { nameof(ReportSalesOrderByEmployeeAndItem_ReportSalesOrderByEmployeeAndItemFilterDTO.OrganizationId), FieldTypeEnum.ID.Id },
+        };
+
+        public static Dictionary<string, List<string>> Action = new Dictionary<string, List<string>>
+        {
+            { "Tìm kiếm", new List<string> {
+                Master, Count, List, Total, Export, FilterListOrganization, FilterListAppUser } },
+
+        };
+    }
+}
---
+++ DMS/Rpc/reports/report-sales-order/report-sales-order-by-employee-and-item/ReportSalesOrderByEmployeeAndItem_ReportSalesOrderByEmployeeAndItemDTO.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.reports.report_sales_order.report_sales_order_by_employee_and_item
+{
+    public class ReportSalesOrderByEmployeeAndItem_ReportSalesOrderByEmployeeAndItemDTO : DataDTO
+    {
+        public string OrganizationName { get; set; }
+        public List<ReportSalesOrderByEmployeeAndItem_SaleEmployeeDTO> SaleEmployees { get; set; }
+    }
+
+    public class ReportSalesOrderByEmployeeAndItem_ReportSalesOrderByEmployeeAndItemFilterDTO : FilterDTO
+    {
+        public IdFilter OrganizationId { get; set; }
+        public IdFilter SaleEmployeeId { get; set; }
+        public DateFilter OrderDate { get; set; }
+    }
+}
