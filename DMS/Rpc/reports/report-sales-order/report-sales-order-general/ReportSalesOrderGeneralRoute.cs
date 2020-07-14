10d9d4fe49fcfba357eea9f29119fb4c7f78956c
hungvimanh <hungvimanh.cntt@gmail.com>
2020-07-14 13:55:29 +0700

    hoàn thiện báo cáo bán hàng tổng hợp

---
+++ DMS/Rpc/reports/report-sales-order/report-sales-order-general/ReportSalesOrderGeneralRoute.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.reports.report_sales_order.report_sales_order_general
+{
+    public class ReportSalesOrderGeneralRoute : Root
+    {
+        public const string Master = Module + "/report-sales-order-by-store-and-item/report-sales-order-by-store-and-item-master";
+
+        private const string Default = Rpc + Module + "/report-sales-order-by-store-and-item";
+        public const string Count = Default + "/count";
+        public const string List = Default + "/list";
+        public const string Total = Default + "/total";
+        public const string Export = Default + "/export";
+
+        public const string FilterListAppUser = Default + "/filter-list-app-user";
+        public const string FilterListOrganization = Default + "/filter-list-organization";
+        public const string FilterListStore = Default + "/filter-list-store";
+
+        public static Dictionary<string, long> Filters = new Dictionary<string, long>
+        {
+            { nameof(ReportSalesOrderGeneral_ReportSalesOrderGeneralFilterDTO.OrganizationId), FieldTypeEnum.ID.Id },
+        };
+
+        public static Dictionary<string, List<string>> Action = new Dictionary<string, List<string>>
+        {
+            { "Tìm kiếm", new List<string> {
+                Master, Count, List, Total, Export, FilterListAppUser, FilterListOrganization, FilterListStore } },
+
+        };
+    }
+}
