147e787b955f7ea4f533877f101eb70533344210
LeDucThang <leducthang1991@gmail.com>
2020-07-14 11:23:23 +0700

    sua lai filter app user cua kpi item

--- DMS/Services/MKpiItem/KpiItemService.cs
+++ DMS/Services/MKpiItem/KpiItemService.cs
@@ -288,18 +288,8 @@ namespace DMS.Services.MKpiItem
 
                 var KpiItems = await UOW.KpiItemRepository.List(KpiItemFilter);
                 var AppUserIds = KpiItems.Select(x => x.EmployeeId).ToList();
-                AppUserFilter = new AppUserFilter
-                {
-                    Skip = 0,
-                    Take = int.MaxValue,
-                    Id = new IdFilter { NotIn = AppUserIds },
-                    Selects = AppUserSelect.Id | AppUserSelect.Username | AppUserSelect.DisplayName | AppUserSelect.Phone | AppUserSelect.Email,
-                    DisplayName = AppUserFilter.DisplayName,
-                    Username = AppUserFilter.Username,
-                    Phone = AppUserFilter.Phone,
-                    Email = AppUserFilter.Email,
-                    OrganizationId = AppUserFilter.OrganizationId
-                };
+                AppUserFilter.Id = new IdFilter { NotIn = AppUserIds };
+                AppUserFilter.Selects = AppUserSelect.Id | AppUserSelect.Username | AppUserSelect.DisplayName | AppUserSelect.Phone | AppUserSelect.Email;
 
                 var AppUsers = await UOW.AppUserRepository.List(AppUserFilter);
                 return AppUsers;
@@ -336,15 +326,8 @@ namespace DMS.Services.MKpiItem
 
                 var KpiItems = await UOW.KpiItemRepository.List(KpiItemFilter);
                 var AppUserIds = KpiItems.Select(x => x.EmployeeId).ToList();
-                AppUserFilter = new AppUserFilter
-                {
-                    Id = new IdFilter { NotIn = AppUserIds },
-                    DisplayName = AppUserFilter.DisplayName,
-                    Username = AppUserFilter.Username,
-                    Phone = AppUserFilter.Phone,
-                    Email = AppUserFilter.Email,
-                    OrganizationId = AppUserFilter.OrganizationId,
-                };
+
+                AppUserFilter.Id = new IdFilter { NotIn = AppUserIds };
 
                 var count = await UOW.AppUserRepository.Count(AppUserFilter);
                 return count;
