73a27c49606e2f800de63c119346f502ffbeb36b
hungvimanh <hungvimanh.cntt@gmail.com>
2020-07-28 11:25:02 +0700

    fix

--- DMS/Rpc/kpi-tracking/kpi-item-report/KpiItemReportController.cs
+++ DMS/Rpc/kpi-tracking/kpi-item-report/KpiItemReportController.cs
@@ -335,7 +335,7 @@ namespace DMS.Rpc.kpi_tracking.kpi_item_report
                     ItemContent.IndirectQuantityPlanned = KpiItemReport_KpiItemContentKpiCriteriaItemMappingDTOs
                             .Where(x => x.SaleEmployeeId == ItemContent.SaleEmployeeId &&
                             x.ItemId == ItemContent.ItemId &&
-                            x.KpiCriteriaItemId == KpiCriteriaItemEnum.INDIRECT_AMOUNT.Id)
+                            x.KpiCriteriaItemId == KpiCriteriaItemEnum.INDIRECT_QUANTITY.Id)
                             .Select(x => x.Value).FirstOrDefault();
                     //thực hiện
                     foreach (var IndirectSalesOrder in IndirectSalesOrders)
