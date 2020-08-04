975a1e1413b5a520cc01d767c298d35cd2c17c85
hungvimanh <hungvimanh.cntt@gmail.com>
2020-08-04 10:38:25 +0700

    map thiếu url

--- DMS/Rpc/dashboards/user/DashboardUser_CommentDTO.cs
+++ DMS/Rpc/dashboards/user/DashboardUser_CommentDTO.cs
@@ -8,10 +8,10 @@ namespace DMS.Rpc.dashboards.user
 {
     public class DashboardUser_CommentDTO : DataDTO
     {
-        public long Id { get; set; }
         public string Avatar { get; set; }
         public string AppUserName { get; set; }
         public string Content { get; set; }
+        public string Url { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 
