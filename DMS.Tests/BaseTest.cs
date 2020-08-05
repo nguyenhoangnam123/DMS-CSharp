9233df64f6dfd35f4635409273580d8d2cdba15c
LeDucThang <leducthang1991@gmail.com>
2020-08-05 16:58:28 +0700

    commit tạm

--- DMS.Tests/BaseTest.cs
+++ DMS.Tests/BaseTest.cs
@@ -1,18 +1,40 @@
+using Common;
+using DMS.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Options;
 using NUnit.Framework;
+using Z.EntityFramework.Extensions;
 
 namespace DMS.Tests
 {
     public class BaseTest
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        public DataContext DataContext;
+        protected ServiceProvider provider;
+        protected ICurrentContext currentContext;
 
-        [Test]
-        public void Test1()
+        public BaseTest()
         {
-            Assert.Pass();
+            var config = new ConfigurationBuilder()
+             .AddJsonFile("appsettings.json")
+             .Build();
+            IHostEnvironment env = new HostingEnvironment();
+            env.EnvironmentName = "Development";
+            Startup startup = new Startup(env);
+            IServiceCollection serviceCollection = new ServiceCollection();
+            startup.ConfigureServices(serviceCollection);
+            provider = serviceCollection.BuildServiceProvider();
+            DataContext = provider.GetService<DataContext>();
+            currentContext = provider.GetService<ICurrentContext>();
+
+        }
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
         }
     }
 }
