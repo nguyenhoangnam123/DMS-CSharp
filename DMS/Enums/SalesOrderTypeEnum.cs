a4ac2fb8fd16fb9db7b5da554df185bdf36a2dd7
hungvimanh <hungvimanh.cntt@gmail.com>
2020-08-06 16:31:03 +0700

    fix duplicate PK

--- DMS/Enums/SalesOrderTypeEnum.cs
+++ DMS/Enums/SalesOrderTypeEnum.cs
@@ -7,7 +7,7 @@ namespace DMS.Enums
     {
         public static GenericEnum ALL = new GenericEnum { Id = 1, Code = "ALL", Name = "Tất cả đơn hàng" };
         public static GenericEnum INDIRECT = new GenericEnum { Id = 2, Code = "INDIRECT", Name = "Đơn hàng gián tiếp" };
-        public static GenericEnum DIRECT = new GenericEnum { Id = 2, Code = "DIRECT", Name = "Đơn hàng trực tiếp" };
+        public static GenericEnum DIRECT = new GenericEnum { Id = 3, Code = "DIRECT", Name = "Đơn hàng trực tiếp" };
         public static List<GenericEnum> SalesOrderTypeEnumList = new List<GenericEnum>()
         {
             ALL, INDIRECT, DIRECT
