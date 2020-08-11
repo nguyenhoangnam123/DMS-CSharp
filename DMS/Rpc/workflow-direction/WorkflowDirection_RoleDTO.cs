cff0dbb2ed019a1b4e1e3e1aea9bcf3c47b6c7f2
LeDucThang <leducthang1991@gmail.com>
2020-08-11 14:23:51 +0700

    trả thêm role cho direction

---
+++ DMS/Rpc/workflow-direction/WorkflowDirection_RoleDTO.cs
@@ -0,0 +1,47 @@
+using Common;
+using DMS.Entities;
+
+namespace DMS.Rpc.workflow_direction
+{
+    public class WorkflowDirection_RoleDTO : DataDTO
+    {
+
+        public long Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public long StatusId { get; set; }
+
+
+        public WorkflowDirection_RoleDTO() { }
+        public WorkflowDirection_RoleDTO(Role Role)
+        {
+
+            this.Id = Role.Id;
+
+            this.Code = Role.Code;
+
+            this.Name = Role.Name;
+
+            this.StatusId = Role.StatusId;
+
+            this.Errors = Role.Errors;
+        }
+    }
+
+    public class WorkflowDirection_RoleFilterDTO : FilterDTO
+    {
+
+        public IdFilter Id { get; set; }
+
+        public StringFilter Code { get; set; }
+
+        public StringFilter Name { get; set; }
+
+        public IdFilter StatusId { get; set; }
+
+        public RoleOrder OrderBy { get; set; }
+    }
+}
