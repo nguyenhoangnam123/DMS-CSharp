e74984949303893c387a802316aa8c73c075d7b2
hungvimanh <hungvimanh.cntt@gmail.com>
2020-08-21 11:55:55 +0700

    fix thông báo

--- DMS/Services/MIndirectSalesOrder/IndirectSalesOrderService.cs
+++ DMS/Services/MIndirectSalesOrder/IndirectSalesOrderService.cs
@@ -799,15 +799,15 @@ namespace DMS.Services.MIndirectSalesOrder
                 .Select(x => x.Id)
                 .ToList();
 
-            var AppUserIds = (await UOW.AppUserRepository.List(new AppUserFilter
+            var AppUsers = await UOW.AppUserRepository.List(new AppUserFilter
             {
                 Skip = 0,
                 Take = int.MaxValue,
-                Selects = AppUserSelect.Id,
+                Selects = AppUserSelect.Id | AppUserSelect.Organization,
                 OrganizationId = new IdFilter { In = OrganizationIds },
                 StatusId = new IdFilter { Equal = StatusEnum.ACTIVE.Id }
-            })).Select(x => x.Id).ToList();
-
+            });
+            var AppUserIds = AppUsers.Where(x => OrganizationIds.Contains(x.OrganizationId.Value)).Select(x => x.Id).ToList();
             AppUserIds = AppUserIds.Intersect(Ids).ToList();
             return AppUserIds;
         }
