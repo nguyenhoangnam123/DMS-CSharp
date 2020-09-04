4fbbd95ed7e49313a30628dbae676a238ced147a
hungvimanh <hungvimanh.cntt@gmail.com>
2020-09-04 14:38:51 +0700

    fix báo cáo thống kê cửa hàng cắm cờ

--- DMS/Rpc/reports/report-statistic/report-statistic-store-scouting/ReportStatisticStoreScouting_Total.cs
+++ DMS/Rpc/reports/report-statistic/report-statistic-store-scouting/ReportStatisticStoreScouting_Total.cs
@@ -13,12 +13,12 @@ namespace DMS.Rpc.reports.report_statistic.report_statistic_store_scouting
         public long StoreOpennedCounter { get; set; }
         public long StoreScoutingUnOpen => StoreScoutingCounter - StoreOpennedCounter;
         public long StoreCounter { get; set; }
-        public decimal? StoreCoutingOpennedRate => StoreScoutingCounter == 0 ? null : (decimal?)((StoreOpennedCounter / StoreScoutingCounter)*100);
-        public string eStoreCoutingOpennedRate => StoreCoutingOpennedRate.HasValue == false ? "" : $"{Math.Round(StoreCoutingOpennedRate.Value, 0)}%";
-        public decimal? StoreCoutingRate => (StoreScoutingUnOpen + StoreCounter) == 0 ? null : (decimal?)((StoreScoutingUnOpen / (StoreScoutingUnOpen + StoreCounter))*100);
-        public string eStoreCoutingRate => StoreCoutingRate.HasValue == false ? "" : $"{Math.Round(StoreCoutingRate.Value, 0)}%";
-        public decimal? StoreRate => StoreCoutingRate.HasValue == false ? null : (decimal?)(100 - StoreCoutingRate.Value);
-        public string eStoreRate => StoreRate.HasValue == false ? "" : $"{Math.Round(StoreRate.Value, 0)}%";
+        public decimal StoreCoutingOpennedRate => StoreScoutingCounter == 0 ? 0 : (((decimal)StoreOpennedCounter / StoreScoutingCounter)*100);
+        public string eStoreCoutingOpennedRate =>$"{Math.Round(StoreCoutingOpennedRate, 0)}%";
+        public decimal StoreCoutingRate => (StoreScoutingUnOpen + StoreCounter) == 0 ? 0 : (((decimal)StoreScoutingUnOpen / (StoreScoutingUnOpen + StoreCounter))*100);
+        public string eStoreCoutingRate => $"{Math.Round(StoreCoutingRate, 0)}%";
+        public decimal StoreRate => 100 - StoreCoutingRate;
+        public string eStoreRate => $"{Math.Round(StoreRate, 0)}%";
     }
 
 }
