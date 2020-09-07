ae5aa38f226e6a55d902ff799252785c5c60bc6f
LeDucThang <leducthang1991@gmail.com>
2020-09-07 13:48:02 +0700

    commit

--- DMS/Handlers/RabbitManager.cs
+++ DMS/Handlers/RabbitManager.cs
@@ -12,29 +12,21 @@ namespace DMS.Handlers
 {
     public interface IRabbitManager
     {
-        IModel Get();
         void PublishList<T>(List<EventMessage<T>> message, GenericEnum routeKey) where T : DataEntity;
         void PublishSingle<T>(EventMessage<T> message, GenericEnum routeKey) where T : DataEntity;
     }
     public class RabbitManager : IRabbitManager
     {
-        private readonly IModel _channel;
-
-        private readonly IConnection _connection;
         private readonly DefaultObjectPool<IModel> _objectPool;
 
         public RabbitManager(IPooledObjectPolicy<IModel> objectPolicy)
         {
-            _objectPool = new DefaultObjectPool<IModel>(objectPolicy, Environment.ProcessorCount * 16);
+            _objectPool = new DefaultObjectPool<IModel>(objectPolicy, Environment.ProcessorCount * 4);
         }
 
 
         private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e) { }
 
-        public IModel Get()
-        {
-            return _channel;
-        }
 
         public void PublishList<T>(List<EventMessage<T>> message, GenericEnum routeKey) where T : DataEntity
         {
@@ -43,7 +35,7 @@ namespace DMS.Handlers
             if (message == null)
                 return;
 
-            var channel = _channel;
+            var channel = _objectPool.Get();
 
             try
             {
