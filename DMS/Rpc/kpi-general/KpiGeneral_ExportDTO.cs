cc0b83113405ac8731c87e6e0b5481cf3abada53
hungvimanh <hungvimanh.cntt@gmail.com>
2020-09-11 17:03:51 +0700

    commit

--- DMS/Rpc/kpi-general/KpiGeneral_ExportDTO.cs
+++ DMS/Rpc/kpi-general/KpiGeneral_ExportDTO.cs
@@ -29,40 +29,23 @@ namespace DMS.Rpc.kpi_general
         public string Name { get; set; }
 
         public decimal? M1Value { get; set; }
-        public string M1 => M1Value == null ? "" : M1Value.ToString().Replace(".0000", string.Empty);
         public decimal? M2Value { get; set; }
-        public string M2 => M2Value == null ? "" : M2Value.ToString().Replace(".0000", string.Empty);
         public decimal? M3Value { get; set; }
-        public string M3 => M3Value == null ? "" : M3Value.ToString().Replace(".0000", string.Empty);
         public decimal? M4Value { get; set; }
-        public string M4 => M4Value == null ? "" : M4Value.ToString().Replace(".0000", string.Empty);
         public decimal? M5Value { get; set; }
-        public string M5 => M5Value == null ? "" : M5Value.ToString().Replace(".0000", string.Empty);
         public decimal? M6Value { get; set; }
-        public string M6 => M6Value == null ? "" : M6Value.ToString().Replace(".0000", string.Empty);
         public decimal? M7Value { get; set; }
-        public string M7 => M7Value == null ? "" : M7Value.ToString().Replace(".0000", string.Empty);
         public decimal? M8Value { get; set; }
-        public string M8 => M8Value == null ? "" : M8Value.ToString().Replace(".0000", string.Empty);
         public decimal? M9Value { get; set; }
-        public string M9 => M9Value == null ? "" : M9Value.ToString().Replace(".0000", string.Empty);
         public decimal? M10Value { get; set; }
-        public string M10 => M10Value == null ? "" : M10Value.ToString().Replace(".0000", string.Empty);
         public decimal? M11Value { get; set; }
-        public string M11 => M11Value == null ? "" : M11Value.ToString().Replace(".0000", string.Empty);
         public decimal? M12Value { get; set; }
-        public string M12 => M12Value == null ? "" : M12Value.ToString().Replace(".0000", string.Empty);
 
         public decimal? Q1Value { get; set; }
-        public string Q1 => Q1Value == null ? "" : Q1Value.ToString().Replace(".0000", string.Empty);
         public decimal? Q2Value { get; set; }
-        public string Q2 => Q2Value == null ? "" : Q2Value.ToString().Replace(".0000", string.Empty);
         public decimal? Q3Value { get; set; }
-        public string Q3 => Q3Value == null ? "" : Q3Value.ToString().Replace(".0000", string.Empty);
         public decimal? Q4Value { get; set; }
-        public string Q4 => Q4Value == null ? "" : Q4Value.ToString().Replace(".0000", string.Empty);
 
         public decimal? YValue { get; set; }
-        public string Y => YValue == null ? "" : YValue.ToString().Replace(".0000", string.Empty);
     }
 }
