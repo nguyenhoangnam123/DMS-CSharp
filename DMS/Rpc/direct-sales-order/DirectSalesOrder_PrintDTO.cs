8ac4f754e399b1c380693b35078821c3126c01cc
hungvimanh <hungvimanh.cntt@gmail.com>
2020-09-18 09:53:31 +0700

    in đơn hàng trực tiếp

---
+++ DMS/Rpc/direct-sales-order/DirectSalesOrder_PrintDTO.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using DMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.direct_sales_order
+{
+    public class DirectSalesOrder_PrintDTO : DataDTO
+    {
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string PhoneNumber { get; set; }
+        public string StoreAddress { get; set; }
+        public string DeliveryAddress { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        public decimal SubTotal { get; set; }
+        public string SubTotalString { get; set; }
+        public decimal? GeneralDiscountAmount { get; set; }
+        public string Discount { get; set; }
+        public decimal TotalTaxAmount { get; set; }
+        public string Tax { get; set; }
+        public decimal Total { get; set; }
+        public string TotalString { get; set; }
+        public string TotalText { get; set; }
+        public DirectSalesOrder_StoreDTO BuyerStore { get; set; }
+        public DirectSalesOrder_AppUserDTO SaleEmployee { get; set; }
+        public List<DirectSalesOrder_PrintContentDTO> Contents { get; set; }
+        public List<DirectSalesOrder_PrintPromotionDTO> Promotions { get; set; }
+        public DirectSalesOrder_PrintDTO() { }
+        public DirectSalesOrder_PrintDTO(DirectSalesOrder DirectSalesOrder)
+        {
+            this.Id = DirectSalesOrder.Id;
+            this.Code = DirectSalesOrder.Code;
+            this.PhoneNumber = DirectSalesOrder.PhoneNumber;
+            this.StoreAddress = DirectSalesOrder.StoreAddress;
+            this.DeliveryAddress = DirectSalesOrder.DeliveryAddress;
+            this.OrderDate = DirectSalesOrder.OrderDate;
+            this.DeliveryDate = DirectSalesOrder.DeliveryDate;
+            this.SubTotal = DirectSalesOrder.SubTotal;
+            this.GeneralDiscountAmount = DirectSalesOrder.GeneralDiscountAmount;
+            this.TotalTaxAmount = DirectSalesOrder.TotalTaxAmount;
+            this.Total = DirectSalesOrder.Total;
+            this.BuyerStore = DirectSalesOrder.BuyerStore == null ? null : new DirectSalesOrder_StoreDTO(DirectSalesOrder.BuyerStore);
+            this.SaleEmployee = DirectSalesOrder.SaleEmployee == null ? null : new DirectSalesOrder_AppUserDTO(DirectSalesOrder.SaleEmployee);
+            this.Contents = DirectSalesOrder.DirectSalesOrderContents?.Select(x => new DirectSalesOrder_PrintContentDTO(x)).ToList();
+            this.Promotions = DirectSalesOrder.DirectSalesOrderPromotions?.Select(x => new DirectSalesOrder_PrintPromotionDTO(x)).ToList();
+            this.Errors = DirectSalesOrder.Errors;
+        }
+    }
+}
---
+++ DMS/Rpc/direct-sales-order/DirectSalesOrder_PrintPromotionDTO.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using DMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.direct_sales_order
+{
+    public class DirectSalesOrder_PrintPromotionDTO : DataDTO
+    {
+        public long STT { get; set; }
+        public long Id { get; set; }
+        public long Quantity { get; set; }
+        public string QuantityString { get; set; }
+        public long RequestedQuantity { get; set; }
+        public string RequestedQuantityString { get; set; }
+        public long? Factor { get; set; }
+        public DirectSalesOrder_ItemDTO Item { get; set; }
+        public DirectSalesOrder_UnitOfMeasureDTO PrimaryUnitOfMeasure { get; set; }
+        public DirectSalesOrder_UnitOfMeasureDTO UnitOfMeasure { get; set; }
+
+        public DirectSalesOrder_PrintPromotionDTO() { }
+        public DirectSalesOrder_PrintPromotionDTO(DirectSalesOrderPromotion DirectSalesOrderPromotion)
+        {
+            this.Id = DirectSalesOrderPromotion.Id;
+            this.Quantity = DirectSalesOrderPromotion.Quantity;
+            this.RequestedQuantity = DirectSalesOrderPromotion.RequestedQuantity;
+            this.Factor = DirectSalesOrderPromotion.Factor;
+            this.Item = DirectSalesOrderPromotion.Item == null ? null : new DirectSalesOrder_ItemDTO(DirectSalesOrderPromotion.Item);
+            this.PrimaryUnitOfMeasure = DirectSalesOrderPromotion.PrimaryUnitOfMeasure == null ? null : new DirectSalesOrder_UnitOfMeasureDTO(DirectSalesOrderPromotion.PrimaryUnitOfMeasure);
+            this.UnitOfMeasure = DirectSalesOrderPromotion.UnitOfMeasure == null ? null : new DirectSalesOrder_UnitOfMeasureDTO(DirectSalesOrderPromotion.UnitOfMeasure);
+            this.Errors = DirectSalesOrderPromotion.Errors;
+        }
+    }
+}
