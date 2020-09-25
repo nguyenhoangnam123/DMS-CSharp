02050ddcd9bbb62cfdafea03144c6605c8588234
LeDucThang <leducthang1991@gmail.com>
2020-09-25 14:21:24 +0700

    commit

---
+++ DMS/Rpc/store/Store_ImportDTO.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.store
+{
+    public class Store_ImportDTO : DataDTO
+    {
+    }
+}
