8dc4bb4f551210b791a1a93649a29fc7cf5f4267
hungvimanh <hungvimanh.cntt@gmail.com>
2020-10-09 09:56:20 +0700

    commit

--- DMS/Services/MDirectSalesOrder/DirectSalesOrderService.cs
+++ DMS/Services/MDirectSalesOrder/DirectSalesOrderService.cs
@@ -209,7 +209,7 @@ namespace DMS.Services.MDirectSalesOrder
                     {
                         TitleWeb = $"Thông báo từ DMS",
                         ContentWeb = $"Đơn hàng {DirectSalesOrder.Code} đã được thêm mới lên hệ thống bởi {CurrentUser.DisplayName}",
-                        LinkWebsite = $"{DirectSalesOrderRoute.Master}/?id=*".Replace("*", DirectSalesOrder.Id.ToString()),
+                        LinkWebsite = $"{DirectSalesOrderRoute.Master}/id=*".Replace("*", DirectSalesOrder.Id.ToString()),
                         LinkMobile = $"{DirectSalesOrderRoute.Mobile}".Replace("*", DirectSalesOrder.Id.ToString()),
                         Time = Now,
                         Unread = true,
@@ -267,7 +267,7 @@ namespace DMS.Services.MDirectSalesOrder
                 {
                     TitleWeb = $"Thông báo từ DMS",
                     ContentWeb = $"Đơn hàng {DirectSalesOrder.Code} đã được cập nhật thông tin bởi {CurrentUser.DisplayName}",
-                    LinkWebsite = $"{DirectSalesOrderRoute.Master}/?id=*".Replace("*", DirectSalesOrder.Id.ToString()),
+                    LinkWebsite = $"{DirectSalesOrderRoute.Master}/id=*".Replace("*", DirectSalesOrder.Id.ToString()),
                     LinkMobile = $"{DirectSalesOrderRoute.Mobile}".Replace("*", DirectSalesOrder.Id.ToString()),
                     Time = Now,
                     Unread = true,
@@ -753,10 +753,10 @@ namespace DMS.Services.MDirectSalesOrder
                 {
                     foreach (var OrganizationId in OrganizationIds)
                     {
-                        long targetPrice = long.MaxValue;
+                        decimal targetPrice = decimal.MaxValue;
                         targetPrice = PriceListItemMappings.Where(x => x.ItemId == ItemId && x.PriceList.OrganizationId == OrganizationId)
                             .Select(x => x.Price)
-                            .DefaultIfEmpty(long.MaxValue)
+                            .DefaultIfEmpty(decimal.MaxValue)
                             .Min();
                         if (targetPrice < result[ItemId])
                         {
@@ -784,10 +784,10 @@ namespace DMS.Services.MDirectSalesOrder
                 {
                     foreach (var OrganizationId in OrganizationIds)
                     {
-                        long targetPrice = long.MinValue;
+                        decimal targetPrice = decimal.MinValue;
                         targetPrice = PriceListItemMappings.Where(x => x.ItemId == ItemId && x.PriceList.OrganizationId == OrganizationId)
                             .Select(x => x.Price)
-                            .DefaultIfEmpty(long.MinValue)
+                            .DefaultIfEmpty(decimal.MinValue)
                             .Max();
                         if (targetPrice > result[ItemId])
                         {
