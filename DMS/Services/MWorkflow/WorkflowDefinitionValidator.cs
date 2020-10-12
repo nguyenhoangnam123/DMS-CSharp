a9a7deb3ceb86bab8875a03d738e30ae8db3b67f
LeDucThang <leducthang1991@gmail.com>
2020-10-12 14:24:35 +0700

    fix lỗi trong validator của definition

--- DMS/Services/MWorkflow/WorkflowDefinitionValidator.cs
+++ DMS/Services/MWorkflow/WorkflowDefinitionValidator.cs
@@ -109,8 +109,7 @@ namespace DMS.Services.MWorkflow
         {
             if (WorkflowDefinition.WorkflowTypeId == 0)
                 WorkflowDefinition.AddError(nameof(WorkflowDefinitionValidator), nameof(WorkflowDefinition.WorkflowType), ErrorCode.WorkflowTypeEmpty);
-            else if (WorkflowDefinition.WorkflowTypeId != WorkflowTypeEnum.INDIRECT_SALES_ORDER.Id && WorkflowDefinition.WorkflowTypeId != WorkflowTypeEnum.PRICE_LIST.Id &&
-                WorkflowDefinition.WorkflowTypeId != WorkflowTypeEnum.EROUTE.Id && WorkflowDefinition.WorkflowTypeId != WorkflowTypeEnum.STORE.Id)
+            else if (!WorkflowTypeEnum.WorkflowTypeEnumList.Any(x => x.Id == WorkflowDefinition.WorkflowTypeId))
                 WorkflowDefinition.AddError(nameof(WorkflowDefinitionValidator), nameof(WorkflowDefinition.WorkflowType), ErrorCode.WorkflowTypeNotExisted);
             return WorkflowDefinition.IsValidated;
         }
