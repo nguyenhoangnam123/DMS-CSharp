769ac91d5304b68a3f0fbd84cb15386561709c47
hungvimanh <hungvimanh.cntt@gmail.com>
2020-10-14 17:23:11 +0700

    commit

--- DMS/Rpc/kpi-tracking/kpi-general-employee-report/KpiGeneralEmployeeReportController.cs
+++ DMS/Rpc/kpi-tracking/kpi-general-employee-report/KpiGeneralEmployeeReportController.cs
@@ -405,7 +405,7 @@ namespace DMS.Rpc.kpi_tracking.kpi_general_employee_report
                     }
                     Period.SkuIndirectOrder = IndirectSalesOrders.Count() == 0 ? null :
                         (decimal?)
-                        Math.Round(Period.SKUIndirectItems.Count() / Period.TotalIndirectOrders.Value, 2);
+                        Math.Round(Period.SKUIndirectItems.Count() / IndirectSalesOrders.Count(), 2);
                     if (Period.SkuIndirectOrderPlanned == null)
                         Period.SkuIndirectOrder = null;
                     //tỉ lệ
@@ -488,7 +488,7 @@ namespace DMS.Rpc.kpi_tracking.kpi_general_employee_report
                     }
                     Period.SkuDirectOrder = IndirectSalesOrders.Count() == 0 ? null :
                         (decimal?)
-                        Math.Round(Period.SKUIndirectItems.Count() / Period.TotalDirectOrders.Value, 2);
+                        Math.Round(Period.SKUIndirectItems.Count() / IndirectSalesOrders.Count(), 2);
                     if (Period.SkuDirectOrderPlanned == null)
                         Period.SkuDirectOrder = null;
                     //tỉ lệ
