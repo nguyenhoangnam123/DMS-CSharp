d4e687e6bdd5016cb33664e269153ed6ff64c8ff
namnh <namnh21894@gmail.com>
2020-10-15 16:29:12 +0700

    add Direct getDetail requestState

--- DMS/Repositories/DirectSalesOrderRepository.cs
+++ DMS/Repositories/DirectSalesOrderRepository.cs
@@ -623,6 +623,8 @@ namespace DMS.Repositories
                 DeliveryAddress = x.DeliveryAddress,
                 SaleEmployeeId = x.SaleEmployeeId,
                 OrderDate = x.OrderDate,
+                CreatedAt = x.CreatedAt,
+                UpdatedAt = x.UpdatedAt,
                 DeliveryDate = x.DeliveryDate,
                 EditedPriceStatusId = x.EditedPriceStatusId,
                 Note = x.Note,
