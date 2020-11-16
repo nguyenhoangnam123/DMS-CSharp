9509b37a69632b1566e430f2826347d88a1aa1c0
hungvimanh <hungvimanh.cntt@gmail.com>
2020-11-16 10:39:29 +0700

    commit

--- DMS/Services/MStore/StoreService.cs
+++ DMS/Services/MStore/StoreService.cs
@@ -200,9 +200,6 @@ namespace DMS.Services.MStore
                 await UOW.Begin();
                 await UOW.StoreRepository.Create(Store);
 
-                if (Store.AppUserId.HasValue)
-                    await UOW.AppUserStoreMappingRepository.Update(Store.AppUserId.Value, Store.Id);
-
                 List<UserNotification> UserNotifications = new List<UserNotification>();
                 if (Store.StoreScoutingId.HasValue)
                 {
