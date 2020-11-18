1273b407ecba2f2351a6068df1e27ef049987300
LeDucThang <leducthang1991@gmail.com>
2020-11-18 16:35:32 +0700

    commit

--- DMS/Startup.cs
+++ DMS/Startup.cs
@@ -191,7 +191,7 @@ namespace DMS
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseAllElasticApm(Configuration);
+            //app.UseAllElasticApm(Configuration);
             app.UseRouting();
             app.UseMiddleware<ErrorHandlingMiddleware>();
             app.UseGleamTech();
