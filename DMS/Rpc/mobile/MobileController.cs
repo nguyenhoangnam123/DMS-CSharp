e345cc5963d53d7ea9fa3ee8a1d67373fdc840ff
hungvimanh <hungvimanh.cntt@gmail.com>
2020-12-14 10:28:18 +0700

    commit

--- DMS/Rpc/mobile/MobileController.cs
+++ DMS/Rpc/mobile/MobileController.cs
@@ -292,7 +292,7 @@ namespace DMS.Rpc.mobile
                 return BadRequest(Mobile_IndirectSalesOrderDTO);
         }
 
-        [Route(MobileRoute.PreviewIndirectOrder), HttpGet]
+        [Route(MobileRoute.PreviewIndirectOrder), HttpPost]
         public async Task<ActionResult> PreviewIndirectOrder([FromBody] Mobile_IndirectSalesOrderDTO Mobile_IndirectSalesOrderDTO)
         {
             if (!ModelState.IsValid)
