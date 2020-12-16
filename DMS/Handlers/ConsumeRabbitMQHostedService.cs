bc15296a32b0ab8082b0caad720f67abe8250196
hungvimanh <hungvimanh.cntt@gmail.com>
2020-12-16 15:36:46 +0700

    commit

--- DMS/Handlers/ConsumeRabbitMQHostedService.cs
+++ DMS/Handlers/ConsumeRabbitMQHostedService.cs
@@ -20,62 +20,68 @@ namespace DMS.Handlers
         private IModel _channel;
         private IConfiguration Configuration;
         private readonly DefaultObjectPool<IModel> _objectPool;
-
+        private IRabbitManager RabbitManager;
         internal List<IHandler> Handlers = new List<IHandler>();
-
         public ConsumeRabbitMQHostedService(IPooledObjectPolicy<IModel> objectPolicy, IConfiguration Configuration)
         {
-            this.Configuration = Configuration;
-            string exchangeName = "exchange";
-            _objectPool = new DefaultObjectPool<IModel>(objectPolicy, Environment.ProcessorCount * 1);
-            _channel = _objectPool.Get();
-            _channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, true, false);
-            _channel.QueueDeclare(StaticParams.ModuleName, true, false, false, null);
-
-            List<Type> handlerTypes = typeof(ConsumeRabbitMQHostedService).Assembly.GetTypes()
-             .Where(x => typeof(Handler).IsAssignableFrom(x) && x.IsClass && !x.IsAbstract)
-             .ToList();
-            foreach (Type type in handlerTypes)
+            if (StaticParams.EnableExternalService)
             {
-                Handler handler = (Handler)Activator.CreateInstance(type);
-                Handlers.Add(handler);
+                this.Configuration = Configuration;
+                string exchangeName = "exchange";
+                _objectPool = new DefaultObjectPool<IModel>(objectPolicy, Environment.ProcessorCount);
+                RabbitManager = new RabbitManager(objectPolicy);
+                _channel = _objectPool.Get();
+                _channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, true, false);
+                _channel.QueueDeclare(StaticParams.ModuleName, true, false, false, null);
+
+                List<Type> handlerTypes = typeof(ConsumeRabbitMQHostedService).Assembly.GetTypes()
+                    .Where(x => typeof(Handler).IsAssignableFrom(x) && x.IsClass && !x.IsAbstract)
+                    .ToList();
+                foreach (Type type in handlerTypes)
+                {
+                    Handler handler = (Handler)Activator.CreateInstance(type);
+                    Handlers.Add(handler);
+                }
+
+                foreach (IHandler handler in Handlers)
+                {
+                    handler.QueueBind(_channel, StaticParams.ModuleName, exchangeName);
+                }
+                _channel.BasicQos(0, 1, false);
             }
-
-            foreach (IHandler handler in Handlers)
-            {
-                handler.QueueBind(_channel, StaticParams.ModuleName, exchangeName);
-            }    
-            _channel.BasicQos(0, 1, false);
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            stoppingToken.ThrowIfCancellationRequested();
-
-            var consumer = new EventingBasicConsumer(_channel);
-            consumer.Received += (ch, ea) => 
+            if (StaticParams.EnableExternalService)
             {
-                // received message  
-                var content = System.Text.Encoding.UTF8.GetString(ea.Body.ToArray());
-                var routingKey = ea.RoutingKey;
-                // handle the received message  
-                try
-                {
-                    _= HandleMessage(routingKey, content);
-                    _channel.BasicAck(ea.DeliveryTag, false);
-                }
-                catch
-                {
-                    _channel.BasicNack(ea.DeliveryTag, false, true);
-                }
-            };
+                stoppingToken.ThrowIfCancellationRequested();
 
-            consumer.Shutdown += OnConsumerShutdown;
-            consumer.Registered += OnConsumerRegistered;
-            consumer.Unregistered += OnConsumerUnregistered;
-            consumer.ConsumerCancelled += OnConsumerCancelled;
+                var consumer = new EventingBasicConsumer(_channel);
+                consumer.Received += (ch, ea) =>
+                {
+                    // received message  
+                    var content = System.Text.Encoding.UTF8.GetString(ea.Body.ToArray());
+                    var routingKey = ea.RoutingKey;
+                    // handle the received message  
+                    try
+                    {
+                        _ = HandleMessage(routingKey, content);
+                        _channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception e)
+                    {
+                        _channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                };
 
-            _channel.BasicConsume(StaticParams.ModuleName, false, consumer);
+                consumer.Shutdown += OnConsumerShutdown;
+                consumer.Registered += OnConsumerRegistered;
+                consumer.Unregistered += OnConsumerUnregistered;
+                consumer.ConsumerCancelled += OnConsumerCancelled;
+
+                _channel.BasicConsume(StaticParams.ModuleName, false, consumer);
+            }
             return Task.CompletedTask;
         }
 
@@ -84,15 +90,17 @@ namespace DMS.Handlers
             var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
             optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DataContext"));
             DataContext context = new DataContext(optionsBuilder.Options);
-           
             List<string> path = routingKey.Split(".").ToList();
             if (path.Count < 1)
                 throw new Exception();
-            foreach(IHandler handler in Handlers)
+            foreach (IHandler handler in Handlers)
             {
-                if (path[0] == handler.Name)
+                if (path.Any(p => p == handler.Name))
+                {
+                    handler.RabbitManager = RabbitManager;
                     await handler.Handle(context, routingKey, content);
-            }    
+                }
+            }
         }
 
         private void OnConsumerCancelled(object sender, ConsumerEventArgs e) { }
