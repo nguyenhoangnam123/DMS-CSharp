ab74f228e7ab1b1ab49f82f4f27049f44f33b08c
hungvimanh <hungvimanh.cntt@gmail.com>
2020-12-16 10:32:11 +0700

    thêm báo cáo

---
+++ DMS/Rpc/monitor/monitor-salesman/MonitorSalesman_ExportUncheckedDTO.cs
@@ -0,0 +1,29 @@
+﻿using DMS.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Rpc.monitor.monitor_salesman
+{
+    public class MonitorSalesman_ExportEmployeeUncheckedDTO : DataDTO
+    {
+        public long AppUserId { get; set; }
+        public string DisplayName { get; set; }
+        public List<MonitorSalesman_ExportUncheckedDTO> Contents { get; set; }
+    }
+    public class MonitorSalesman_ExportUncheckedDTO : DataDTO
+    {
+        public long STT { get; set; }
+        public long AppUserId { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string StoreCode { get; set; }
+        public string StoreCodeDraft { get; set; }
+        public string StoreName { get; set; }
+        public string StorePhone { get; set; }
+        public string StoreAddress { get; set; }
+        public string StoreTypeName { get; set; }
+        public string StoreStatusName { get; set; }
+    }
+}
