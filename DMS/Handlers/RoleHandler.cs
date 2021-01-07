71e6cc08bad55fb4704c2a3faa745e0bd16dc6ff
hungvimanh <hungvimanh.cntt@gmail.com>
2021-01-07 16:21:41 +0700

    commit

--- DMS/Handlers/RoleHandler.cs
+++ DMS/Handlers/RoleHandler.cs
@@ -13,60 +13,18 @@ namespace DMS.Handlers
 {
     public class RoleHandler : Handler
     {
-        private string SyncKey => Name + ".Sync";
-        private string UsedKey => Name + ".Used";
+        private string UsedKey => $"DMS.{Name}.Used";
         public override string Name => nameof(Role);
 
         public override void QueueBind(IModel channel, string queue, string exchange)
         {
-            channel.QueueBind(queue, exchange, $"{Name}.*", null);
+            channel.QueueBind(queue, exchange, $"DMS.{Name}.*", null);
         }
         public override async Task Handle(DataContext context, string routingKey, string content)
         {
-            if (routingKey == SyncKey)
-                await Sync(context, content);
             if (routingKey == UsedKey)
                 await Used(context, content);
         }
-
-        private async Task Sync(DataContext context, string json)
-        {
-            List<EventMessage<Role>> EventMessageReviced = JsonConvert.DeserializeObject<List<EventMessage<Role>>>(json);
-            await SaveEventMessage(context, SyncKey, EventMessageReviced);
-            List<Guid> RowIds = EventMessageReviced.Select(a => a.RowId).Distinct().ToList();
-            List<EventMessage<Role>> RoleEventMessages = await ListEventMessage<Role>(context, SyncKey, RowIds);
-
-            List<Role> Roles = new List<Role>();
-            foreach (var RowId in RowIds)
-            {
-                EventMessage<Role> EventMessage = RoleEventMessages.Where(e => e.RowId == RowId).OrderByDescending(e => e.Time).FirstOrDefault();
-                if (EventMessage != null)
-                    Roles.Add(EventMessage.Content);
-            }
-            try
-            {
-                List<AppUserRoleMappingDAO> AppUserRoleMappingDAOs = new List<AppUserRoleMappingDAO>();
-                foreach (Role Role in Roles)
-                {
-                    RoleDAO RoleDAO = await context.Role.Where(r => r.Code == Role.Code).FirstOrDefaultAsync();
-                    if (RoleDAO != null)
-                    {
-                        AppUserRoleMappingDAOs.AddRange(Role.AppUserRoleMappings.Select(ar => new AppUserRoleMappingDAO
-                        {
-                            RoleId = RoleDAO.Id,
-                            AppUserId = ar.AppUserId,
-                        }).ToList());
-                    }
-                }
-
-                await context.BulkMergeAsync(AppUserRoleMappingDAOs);
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-
         private async Task Used(DataContext context, string json)
         {
             List<EventMessage<Role>> EventMessageReviced = JsonConvert.DeserializeObject<List<EventMessage<Role>>>(json);
