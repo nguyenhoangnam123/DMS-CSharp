7ccda4daa22077a4b3774e8f94963ad384c2b7df
namnh <namnh21894@gmail.com>
2021-03-08 14:02:31 +0700

    fix id duplicates

--- DMS/Enums/StoreApprovalStateEnum.cs
+++ DMS/Enums/StoreApprovalStateEnum.cs
@@ -9,8 +9,8 @@ namespace DMS.Enums
     public class StoreApprovalStateEnum
     {
         public static GenericEnum PENDING = new GenericEnum { Id = 1, Code = "PENDING", Name = "Chờ duyệt" };
-        public static GenericEnum APPROVED = new GenericEnum { Id = 1, Code = "APPROVED", Name = "Hoàn thành" };
-        public static GenericEnum REJECTED = new GenericEnum { Id = 1, Code = "REJECTED", Name = "Từ chối" };
+        public static GenericEnum APPROVED = new GenericEnum { Id = 2, Code = "APPROVED", Name = "Hoàn thành" };
+        public static GenericEnum REJECTED = new GenericEnum { Id = 3, Code = "REJECTED", Name = "Từ chối" };
 
 
         public static List<GenericEnum> StoreApprovalStateEnumList = new List<GenericEnum> {
