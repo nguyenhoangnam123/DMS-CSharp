2c0796c66aeaa8fcba63f208ca5072dc5669ae6f
hungvimanh <hungvimanh.cntt@gmail.com>
2021-03-09 14:38:44 +0700

    commit

--- DMS/Rpc/kpi-tracking/kpi-item-report/KpiItemReportController.cs
+++ DMS/Rpc/kpi-tracking/kpi-item-report/KpiItemReportController.cs
@@ -20,6 +20,7 @@ using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DMS.Services.MKpiItemType;
 
 namespace DMS.Rpc.kpi_tracking.kpi_item_report
 {
@@ -31,6 +32,7 @@ namespace DMS.Rpc.kpi_tracking.kpi_item_report
         private IKpiYearService KpiYearService;
         private IKpiPeriodService KpiPeriodService;
         private IItemService ItemService;
+        private IKpiItemTypeService KpiItemTypeService;
         private ICurrentContext CurrentContext;
         public KpiItemReportController(DataContext DataContext,
             IOrganizationService OrganizationService,
@@ -38,6 +40,7 @@ namespace DMS.Rpc.kpi_tracking.kpi_item_report
             IKpiYearService KpiYearService,
             IKpiPeriodService KpiPeriodService,
             IItemService ItemService,
+            IKpiItemTypeService KpiItemTypeService,
             ICurrentContext CurrentContext)
         {
             this.DataContext = DataContext;
@@ -46,6 +49,7 @@ namespace DMS.Rpc.kpi_tracking.kpi_item_report
             this.KpiPeriodService = KpiPeriodService;
             this.KpiYearService = KpiYearService;
             this.ItemService = ItemService;
+            this.KpiItemTypeService = KpiItemTypeService;
             this.CurrentContext = CurrentContext;
         }
 
@@ -166,6 +170,28 @@ namespace DMS.Rpc.kpi_tracking.kpi_item_report
             List<KpiItemReport_KpiYearDTO> KpiItemReport_KpiYearDTOs = KpiYears
                 .Select(x => new KpiItemReport_KpiYearDTO(x)).ToList();
             return KpiItemReport_KpiYearDTOs;
+        }
+
+        [Route(KpiItemReportRoute.FilterListKpiItemType), HttpPost]
+        public async Task<List<KpiItemReport_KpiItemTypeDTO>> FilterListKpiItemType([FromBody] KpiItemReport_KpiItemTypeFilterDTO KpiItemReport_KpiItemTypeFilterDTO)
+        {
+            if (!ModelState.IsValid)
+                throw new BindException(ModelState);
+
+            KpiItemTypeFilter KpiItemTypeFilter = new KpiItemTypeFilter();
+            KpiItemTypeFilter.Skip = 0;
+            KpiItemTypeFilter.Take = 20;
+            KpiItemTypeFilter.OrderBy = KpiItemTypeOrder.Id;
+            KpiItemTypeFilter.OrderType = OrderType.ASC;
+            KpiItemTypeFilter.Selects = KpiItemTypeSelect.ALL;
+            KpiItemTypeFilter.Id = KpiItemReport_KpiItemTypeFilterDTO.Id;
+            KpiItemTypeFilter.Code = KpiItemReport_KpiItemTypeFilterDTO.Code;
+            KpiItemTypeFilter.Name = KpiItemReport_KpiItemTypeFilterDTO.Name;
+
+            List<KpiItemType> KpiItemTypes = await KpiItemTypeService.List(KpiItemTypeFilter);
+            List<KpiItemReport_KpiItemTypeDTO> KpiItemReport_KpiItemTypeDTOs = KpiItemTypes
+                .Select(x => new KpiItemReport_KpiItemTypeDTO(x)).ToList();
+            return KpiItemReport_KpiItemTypeDTOs;
         }
 
         [Route(KpiItemReportRoute.Count), HttpPost]
