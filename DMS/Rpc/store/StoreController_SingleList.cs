4b1929c19ab33273152d3d5629aa84fdfd20fb2a
joseleii <Phambaodu25072001@gmail.com>
2021-03-25 15:32:40 +0700

    cc

--- DMS/Rpc/store/StoreController_SingleList.cs
+++ DMS/Rpc/store/StoreController_SingleList.cs
@@ -264,7 +264,7 @@ namespace DMS.Rpc.store
             OrganizationFilter.OrderType = OrderType.ASC;
             OrganizationFilter.Selects = OrganizationSelect.ALL;
             OrganizationFilter.StatusId = new IdFilter { Equal = StatusEnum.ACTIVE.Id };
-
+            OrganizationFilter.IsDisplay = true;
             if (OrganizationFilter.Id == null) OrganizationFilter.Id = new IdFilter();
             OrganizationFilter.Id.In = await FilterOrganization(OrganizationService, CurrentContext);
 
