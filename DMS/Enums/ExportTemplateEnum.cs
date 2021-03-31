ad750b6f3d22d68af2d81e426cae5aa73a86f0a3
namnh <namnh21894@gmail.com>
2021-03-31 16:25:17 +0700

    add template enum

--- DMS/Enums/ExportTemplateEnum.cs
+++ DMS/Enums/ExportTemplateEnum.cs
@@ -11,10 +11,11 @@ namespace DMS.Enums
         public static GenericEnum PRINT_DIRECT = new GenericEnum { Id = 1, Code = "PRINT_DIRECT", Name = "Mẫu in đơn hàng trực tiếp" };
         public static GenericEnum PRINT_INDIRECT = new GenericEnum { Id = 2, Code = "PRINT_INDIRECT", Name = "Mẫu in đơn hàng gián tiếp" };
         public static GenericEnum PRINT_INDIRECT_MOBILE = new GenericEnum { Id = 3, Code = "PRINT_INDIRECT_MOBILE", Name = "Mẫu in đơn hàng mobile" };
+        public static GenericEnum PRINT_DIRECT_MOBILE = new GenericEnum { Id = 4, Code = "PRINT_DIRECT_MOBILE", Name = "Mẫu in đơn hàng trực tiếp mobile" };
 
         public static List<GenericEnum> ExportTemplateEnumList = new List<GenericEnum>
         {
-            PRINT_DIRECT, PRINT_INDIRECT, PRINT_INDIRECT_MOBILE
+            PRINT_DIRECT, PRINT_INDIRECT, PRINT_INDIRECT_MOBILE, PRINT_DIRECT_MOBILE
         };
     }
 }
