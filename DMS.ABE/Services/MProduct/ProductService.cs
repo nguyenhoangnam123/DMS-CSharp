587e0a16169def7557deb1b9d9356656de16bf7b
namnh <namnh21894@gmail.com>
2021-04-16 15:44:24 +0700

    fix ItemService and ProductService

--- DMS.ABE/Services/MProduct/ProductService.cs
+++ DMS.ABE/Services/MProduct/ProductService.cs
@@ -340,10 +340,11 @@ namespace DMS.ABE.Services.MProduct
             }
 
             //nhân giá với thuế
-            foreach (var item in Items)
-            {
-                item.SalePrice = result[item.Id] * (1 + item.Product.TaxType.Percentage / 100);
-            }
+            //foreach (var item in Items)
+            //{
+            //    item.SalePrice = result[item.Id] * (1 + item.Product.TaxType.Percentage / 100);
+            //}
+            // gia hien thi tren man hinh khong tinh thue
             return Items;
         }
 
