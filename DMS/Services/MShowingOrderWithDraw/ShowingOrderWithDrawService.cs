10db160d7f548e46aa84003c48375b43efadb3ae
namnh <namnh21894@gmail.com>
2021-04-27 09:47:13 +0700

    ShowingOrderWithDraw: fix NotifyUsed

--- DMS/Services/MShowingOrderWithDraw/ShowingOrderWithDrawService.cs
+++ DMS/Services/MShowingOrderWithDraw/ShowingOrderWithDrawService.cs
@@ -317,15 +317,18 @@ namespace DMS.Services.MShowingOrderWithDraw
             }
 
             {
-                List<long> StoreIds = ShowingOrderWithDraw.Stores.Select(x => x.Id).Distinct().ToList();
-                List<EventMessage<Store>> storeMessages = StoreIds.Select(x => new EventMessage<Store>
+                if(ShowingOrderWithDraw.Stores != null)
                 {
-                    Content = new Store { Id = x },
-                    EntityName = nameof(Store),
-                    RowId = Guid.NewGuid(),
-                    Time = StaticParams.DateTimeNow,
-                }).ToList();
-                RabbitManager.PublishList(storeMessages, RoutingKeyEnum.StoreUsed);
+                    List<long> StoreIds = ShowingOrderWithDraw.Stores.Select(x => x.Id).Distinct().ToList();
+                    List<EventMessage<Store>> storeMessages = StoreIds.Select(x => new EventMessage<Store>
+                    {
+                        Content = new Store { Id = x },
+                        EntityName = nameof(Store),
+                        RowId = Guid.NewGuid(),
+                        Time = StaticParams.DateTimeNow,
+                    }).ToList();
+                    RabbitManager.PublishList(storeMessages, RoutingKeyEnum.StoreUsed);
+                }
             }
 
             {
