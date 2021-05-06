067ab1e9aa86ac44042910b4ee78d2e5f8cd8a78
Ngai Hoang Van <>
2021-05-06 16:50:45 +0000

    Updated docker-entrypoint.sh

--- DMS/docker-entrypoint.sh
+++ DMS/docker-entrypoint.sh
@@ -4,6 +4,7 @@ sed -i "s/{SQL_IP}/${SQL_IP}/g" appsettings.json
 sed -i "s/{SQL_DB}/${SQL_DB}/g" appsettings.json
 sed -i "s/{SQL_USER}/${SQL_USER}/g" appsettings.json
 sed -i "s/{SQL_PASS}/${SQL_PASS}/g" appsettings.json
+sed -i "s/{PUBLIC_RSA_KEY}/${PUBLIC_RSA_KEY}/g" appsettings.json
 
 PROJECT_NAME=DMS
 dotnet ${PROJECT_NAME}.dll --urls http://0.0.0.0:8080 --launch-profile ${MODE}
