e96aa1bc8f21b796a67c41d7868d5a573b97e83f
ThangLD <leducthang1991@gmail.com>
2021-05-21 10:12:52 +0700

    commit

--- DMS/Rpc/SetupController.cs
+++ DMS/Rpc/SetupController.cs
@@ -1176,8 +1176,8 @@ namespace DMS.Rpc
             InitField(routeTypes);
             InitAction(routeTypes);
 
-            DataContext.ActionPageMapping.Where(ap => ap.Action.IsDeleted || ap.Page.IsDeleted).DeleteFromQuery();
-            DataContext.PermissionActionMapping.Where(ap => ap.Action.IsDeleted).DeleteFromQuery();
+            DataContext.ActionPageMapping.Where(ap => ap.Action.IsDeleted || ap.Page.IsDeleted || ap.Action.Menu.IsDeleted).DeleteFromQuery();
+            DataContext.PermissionActionMapping.Where(ap => ap.Action.IsDeleted || ap.Action.Menu.IsDeleted).DeleteFromQuery();
             DataContext.Action.Where(p => p.IsDeleted || p.Menu.IsDeleted).DeleteFromQuery();
             DataContext.Page.Where(p => p.IsDeleted).DeleteFromQuery();
             DataContext.PermissionContent.Where(f => f.Field.IsDeleted == true || f.Field.Menu.IsDeleted).DeleteFromQuery();
